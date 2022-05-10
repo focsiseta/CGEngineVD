cee374df3aa88f9c934698c7ec9903881ad77cd5
focsiseta <45460795+focsiseta@users.noreply.github.com>
2022-05-10 19:44:21 +0200

    For some reason webstorm hasn't pushed this file

---
+++ common/support/engine/input.js
@@ -0,0 +1,21 @@
+class Input{
+    constructor(){
+        this.keys = []
+        window.addEventListener("keydown",(event) => {
+            this.keys[event.key] = true
+            event.preventDefault()
+        })
+
+        window.addEventListener("keyup",(event) => {
+            this.keys[event.key] = false
+            event.preventDefault()
+        })
+    }
+    getKeyStatus(key = 'e'){
+        return this.keys[key]
+    }
+
+
+
+
+}
